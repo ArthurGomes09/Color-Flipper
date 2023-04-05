0b1eea8b3a68bec013cbec77cca5da69fbe6aeab
Arthur Gomes <arthur.b@al.estudante.senai.br>
2023-04-05 09:01:01 -0300

    Hex Colors Pronto

---
+++ app.js
@@ -0,0 +1,21 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn")
+const color = document.querySelector(".color")
+
+btn.addEventListener('click', ()=> {
+    console.log(document.body)
+
+    //pegar um número entre 0 e 3
+    const randomNumber = getRandomNumber()
+
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+
+    
+
+} )
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length )
+}
+// console.log(Math.floor(Math.random() * 3))
+console.log(getRandomNumber())
